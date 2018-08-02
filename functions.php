81095727583b217253c0c03a19700d2ae7bdcda0
Phil Barker <phil@pjjk.net>
2018-08-02 15:16:30 +0100

    initial theme scaffolding

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @license GPL 2.0+
+ */
+
+function omniana2_theme_setup() {
+	// Add theme support for special features here.
+}
+
+add_action( 'after_setup_theme', 'omniana2_theme_setup' );
