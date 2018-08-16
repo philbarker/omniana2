81161dac5b24c265cffb88e6a5be7fe4274c1c93
Phil Barker <phil@pjjk.net>
2018-08-16 11:19:47 +0100

    add style sheet for cover page; style top part.

--- functions.php
+++ functions.php
@@ -3,8 +3,30 @@
  * @license GPL 2.0+
  */
 
+function omniana_add_styles() {
+  if ( is_front_page() ) {
+    $src = get_stylesheet_directory_uri() . '/omniana-cover.css';
+    wp_register_style(
+      'omniana_cover',
+      $src,
+      null,
+      null,
+      'screen' // CSS media type
+    );
+    wp_enqueue_style(
+      'omniana_cover',
+       $src,
+       null,
+       null,
+       'screen'
+    );
+  }
+}
+
 function omniana2_theme_setup() {
 	// Add theme support for special features here.
+  add_action('wp_enqueue_scripts', 'omniana_add_styles');
+
 }
 
 add_action( 'after_setup_theme', 'omniana2_theme_setup' );
