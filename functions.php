84d89e889ba250e05a0dab3ae9ab251db735896a
Phil Barker <phil@pjjk.net>
2018-11-20 17:09:39 +0000

    tidying

--- functions.php
+++ functions.php
@@ -26,7 +26,5 @@ function omniana_add_styles() {
 function omniana2_theme_setup() {
 	// Add theme support for special features here.
   add_action('wp_enqueue_scripts', 'omniana_add_styles');
-
 }
-
 add_action( 'after_setup_theme', 'omniana2_theme_setup' );
